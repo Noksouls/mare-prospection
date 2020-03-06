3641e19230b0f5e863b76d48ac996fd5c6b6faf4
Noksouls <noksouls.gamer@gmail.com>
2020-03-06 22:45:23 +0100

    Vérification + modification de la partie inscription

--- lib/inscription.dart
+++ lib/inscription.dart
@@ -11,8 +11,8 @@ class Inscription extends StatelessWidget{
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Column(
-          children: <Widget>[
-          FadeAnimation(1.8, Container(
+        children: <Widget>[
+        FadeAnimation(1.8, Container(
       padding: EdgeInsets.all(5),
       decoration: BoxDecoration(
           color: Colors.white,
@@ -25,7 +25,33 @@ class Inscription extends StatelessWidget{
             )
           ]
       ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    border: Border(bottom: BorderSide(color: Colors.grey[100]))
+            ),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Email ou Pseudo",
+                      hintStyle: TextStyle(color: Colors.grey[400])
+              ),
+            ),
+          ),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Password",
+                      hintStyle: TextStyle(color: Colors.grey[400])
+              ),
+            ),
+          )
+        ],
       ),
-    )])));
+    ))])));
   }
 }
